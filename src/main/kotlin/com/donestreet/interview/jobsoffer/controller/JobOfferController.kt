758e0fdad9799bc34c1dc260434161184acf111e
hercappa <cappa.hernan.javier@gmail.com>
2020-12-23 08:56:59 -0300

    Organize requests.

--- src/main/kotlin/com/donestreet/interview/jobsoffer/controller/JobOfferController.kt
+++ src/main/kotlin/com/donestreet/interview/jobsoffer/controller/JobOfferController.kt
@@ -7,6 +7,7 @@ import org.json.JSONArray
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -19,12 +20,21 @@ class JobOfferController {
     @Autowired
     lateinit var jobsOfferService: JobsOfferService
 
-    @PostMapping()
-    fun search(@RequestBody search: Search):Any{
-        var response = jobsOfferService.fetchgitHubJobsAndSaveSearch(search)
-        println("Resposne: ${response.jsonArray}")
-        //return response.jsonArray
-        return response.text
+    @GetMapping(produces= [MediaType.APPLICATION_JSON_VALUE])
+    fun fetchGitHubJobs(@RequestParam location:String, @RequestParam description:String): ResponseEntity<Any> {
+        val jobs:Response = jobsOfferService.fetchGitHubJobs(description,location)
+        return if (jobs != null) {
+            ResponseEntity(jobs.text,HttpStatus.OK)
+        }else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
     }
+
+    @PostMapping()
+    fun saveSearch(@RequestBody search: Search):ResponseEntity<Unit>{
+        jobsOfferService.save(search)
+        return ResponseEntity(Unit,HttpStatus.CREATED)
+    }
+
 }
 
--- src/main/kotlin/com/donestreet/interview/jobsoffer/service/JobsOfferService.kt
+++ src/main/kotlin/com/donestreet/interview/jobsoffer/service/JobsOfferService.kt
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service
 interface JobsOfferService {
     fun fetchGitHubJobs(description: String, location: String): Response
     fun save(search: Search)
-    fun fetchgitHubJobsAndSaveSearch(search: Search):Response
 }
 
 @Service
@@ -25,16 +24,12 @@ class JobsOfferServiceImpl : JobsOfferService {
 
     override fun fetchGitHubJobs(description:String, location: String ): Response {
         val params = mapOf("description" to description, "location" to location)
-        val response:Response = khttp.get(apiJobUrl,params=params)
-        return response
+        return khttp.get(apiJobUrl,params=params)
     }
 
-    override fun save(search:Search){
+    override fun save(search:Search) {
+        //TODO : Add throws exception to check database connection
         searchRepository.save(search)
     }
 
-    override fun fetchgitHubJobsAndSaveSearch(search:Search):Response{
-        save(search)
-        return fetchGitHubJobs(search.description, search.location)
-    }
 }
